d3de7bb716944a385e25a05bf095e2e652e35e21
Andrii Popov <Popov.Andrey80@gmail.com>
2024-11-24 19:58:59 +0200

    add 1st task

---
+++ main.py
@@ -0,0 +1,5 @@
+from datetime import datetime, timedelta
+
+now = datetime.now()
+future_date = now + timedelta(days=10)  # Додаємо 10 днів до поточної дати
+print(future_date)
---
+++ task-01.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+def get_days_from_today(date):
+    formatted_date = datetime.strptime(date, "%Y-%m-%d")
+    now = datetime.today()
+    delta = now - formatted_date
+    return print(delta.days)
+    
+
+get_days_from_today('2020-10-09')
