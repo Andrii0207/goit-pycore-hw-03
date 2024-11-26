d0732d116b94e0ac89c5777a5b4ff94759e77d52
Andrii Popov <Popov.Andrey80@gmail.com>
2024-11-26 20:50:40 +0200

    fix mentor comments

--- task-01.py
+++ task-01.py
@@ -1,10 +1,16 @@
 from datetime import datetime
 
-def get_days_from_today(date):
-    formatted_date = datetime.strptime(date, "%Y-%m-%d")
-    now = datetime.today()
-    delta = now - formatted_date
-    return print(delta.days)
-    
 
-get_days_from_today("2021-10-09")
+def get_days_from_today(date):
+
+    try:
+        formatted_date = datetime.strptime(date, "%Y-%m-%d")
+        now = datetime.today()
+        delta = now - formatted_date
+
+    except ValueError:
+        print("Неправильний формат дати. Використовуйте 'РРРР-ММ-ДД'.")
+
+    return print(delta.days)
+
+get_days_from_today("2020-10-09")
--- task-02.py
+++ task-02.py
@@ -4,19 +4,27 @@ import random
 def get_numbers_ticket(min, max, quantity):
     k = 0
     result = []
-    while k < quantity:
-        if min < 1 or max > 1000:
-            print ("array must be in range from 1 to 1000")
-            break
 
+    while k < quantity:
+    
+        if (max - min) < quantity:
+            print("You should increase number range or decrease quantity of number")
+            break
+    
+        if min < 1 or max > 1000:
+            print ("Enter numbers must be in range from 1 to 1000")
+            break
+     
         random_number = random.randint(min, max)
+
+        if random_number in result:
+            continue
+        
         result.append(random_number)
         k += 1
-    result.sort()
+        result.sort()
     return result
    
 
-
-
-lottery_numbers =  get_numbers_ticket(1, 1000, 5)
+lottery_numbers =  get_numbers_ticket(2, 8, 5)
 print("Ваші лотерейні числа:", lottery_numbers)
